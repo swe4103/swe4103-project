a168df5b4b2239cfbc20aa1a6d041007d8a38d86
matthew-collett <matthew.collett12@gmail.com>
2024-09-30 20:01:59 -0300

    parking

--- api/__tests__/app.test.js
+++ api/__tests__/app.test.js
@@ -3,7 +3,7 @@ import request from 'supertest'
 import app from '../app/index.js'
 
 describe('Test Express App', () => {
-  it('should respond with a 200 status code on root endpoint', async () => {
+  it('should respond with a 404 status code on root endpoint', async () => {
     const res = await request(app).get('/')
     expect(res.statusCode).toEqual(404)
   })
