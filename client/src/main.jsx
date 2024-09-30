f318d4fa9da3a64f75a4cdc5868a44d9e3167228
Eric599 <ericcuenat@erics-mbp.home>
2024-09-30 13:39:15 -0300

    Add initial front end files

---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
