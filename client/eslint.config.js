9d5feba54716a222dac273f1205cbd2e1d0acecb
Eric599 <ericcuenat@erics-mbp.home>
2024-09-30 14:56:13 -0300

    Start eslint setup

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier' // Add Prettier plugin
+import prettierConfig from 'eslint-config-prettier' // Add Prettier config
 
 export default [
   { ignores: ['dist'] },
@@ -22,7 +24,14 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier, // Include Prettier plugin
     },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended', // Extend Prettier config
+      prettierConfig, // Use Prettier's config to disable conflicting ESLint rules
+    ],
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
@@ -30,6 +39,15 @@ export default [
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': [
+        'error',
+        {
+          arrowParens: 'avoid', // Enforce Prettier rule for arrow functions
+          singleQuote: true, // Enforce single quotes
+          semi: false, // Enforce no semicolons
+          trailingComma: 'all', // Enforce trailing commas
+        },
+      ],
     },
   },
 ]
