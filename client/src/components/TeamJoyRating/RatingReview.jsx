9793da6ecbb969c78caff267fc0895c1dc895170
LucasSavoie <h4etj@unb.ca>
2024-10-04 17:37:20 -0300

    feat(scrum-11): Added team joy feature component - no functionality

---
+++ client/src/components/TeamJoyRating/RatingReview.jsx
@@ -0,0 +1,27 @@
+function RatingReview({ rating, setRating }) {
+  return (
+    <div>
+      {[1, 2, 3, 4, 5].map(star => {
+        return (
+          <span
+            key={star}
+            className="start"
+            style={{
+              cursor: 'pointer',
+              color: rating >= star ? 'gold' : 'gray',
+              fontSize: `35px`,
+            }}
+            onClick={() => {
+              setRating(star)
+            }}
+          >
+            {' '}
+            ★{' '}
+          </span>
+        )
+      })}
+    </div>
+  )
+}
+
+export default RatingReview
