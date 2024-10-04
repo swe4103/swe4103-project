edefd3a9c6d372dbb58cc8b3b01b55d6d38ca4d6
Aidan <aidan@P-s-Z-Flip5.wifi.unb.ca>
2024-10-04 12:28:32 -0300

    feat(SCRUM-21): Added state hooks and event handing to LoginForm and RegistrationForm components.

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,17 +1,77 @@
+import { useState } from 'react'
 import '../../App.css'
 
 const LoginForm = ({ setIsRegister }) => {
+  // State hooks for login email, password
+  const [loginEmail, setLoginEmail] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  // Event handlers for email, passowrd
+  const onEmailChange = event => {
+    setLoginEmail(event.target.value)
+  }
+
+  const onPasswordChange = event => {
+    setLoginPassword(event.target.value)
+  }
+
+  // Event handler for form submission
+  const onSubmitLogin = async event => {
+    // Prevent form from refreshing page
+    event.preventDefault()
+
+    // TODO: Send request to API
+    try {
+      const response = await fetch('endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: loginEmail,
+          password: loginPassword,
+        }),
+      })
+
+      if (!response.ok) {
+        throw new Error('Login failed! Please check your credentials.')
+      }
+
+      const data = await response.json()
+      // Handle successful login (e.g., set user state, redirect, etc.)
+      console.log('Login successful:', data)
+    } catch (error) {
+      setErrorMessage(error.message)
+      return errorMessage
+    }
+  }
+
   return (
     <div className="login-signup-container">
       <h2>Login</h2>
-      <form className="basic-form">
+      <form className="basic-form" onSubmit={onSubmitLogin}>
         <div className="input-group">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Enter your username" required />
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your username"
+            required
+            value={loginEmail}
+            onChange={onEmailChange}
+          />
         </div>
         <div className="input-group">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" placeholder="Enter your password" required />
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            required
+            value={loginPassword}
+            onChange={onPasswordChange}
+          />
         </div>
         <button type="submit" className="rounded-button">
           Login
--- client/src/components/RegistrationForm/RegisterForm.jsx
+++ client/src/components/RegistrationForm/RegisterForm.jsx
@@ -1,25 +1,101 @@
+import { useState } from 'react'
 import '../../App.css'
 
 const RegisterForm = ({ setIsRegister }) => {
+  // State hooks for email, password, and error
+  const [registerEmail, setRegisterEmail] = useState('')
+  const [registerPassword, setRegisterPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  // Event handlers for email, password, confirm password
+  const onEmailChange = event => {
+    setRegisterEmail(event.target.value)
+  }
+
+  const onPasswordChange = event => {
+    setRegisterPassword(event.target.value)
+  }
+
+  const onConfirmPasswordChange = event => {
+    setConfirmPassword(event.target.value)
+  }
+
+  // Event handler for form submission
+  const onSubmitRegister = async event => {
+    // Prevent form from refreshing page
+    event.preventDefault()
+
+    // Check if passwords match
+    if (registerPassword !== confirmPassword) {
+      setErrorMessage('Error: Passwords do not match')
+      alert(errorMessage)
+      return
+    } else {
+      setErrorMessage('')
+    }
+
+    // TODO: Send request to API
+    try {
+      const response = await fetch('endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: registerEmail,
+          password: registerPassword,
+        }),
+      })
+
+      if (!response.ok) {
+        throw new Error('Registration failed! Please try again.')
+      }
+
+      const data = response.json()
+      // TODO: Handle successful registration (e.g., redirect, show message, etc.)
+      console.log('Registration successful:', data)
+    } catch (error) {
+      setErrorMessage(error.message)
+      return errorMessage
+    }
+  }
+
   return (
     <div className="login-signup-container">
       <h2>Register</h2>
-      <form className="basic-form">
+      <form className="basic-form" onSubmit={onSubmitRegister}>
         <div className="input-group">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Create a username" required />
+          <input
+            type="email"
+            id="email"
+            placeholder="Create a username"
+            required
+            value={registerEmail}
+            onChange={onEmailChange}
+          />
         </div>
         <div className="input-group">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" placeholder="Create a password" required />
+          <input
+            type="password"
+            id="password"
+            placeholder="Create a password"
+            required
+            value={registerPassword}
+            onChange={onPasswordChange}
+          />
         </div>
         <div className="input-group">
           <label htmlFor="password-confirm">Confirm Password</label>
           <input
-            type="passwordConfirm"
+            type="password"
             id="passwordConfirm"
             placeholder="Re-enter password"
             required
+            value={confirmPassword}
+            onChange={onConfirmPasswordChange}
           />
         </div>
         <button type="submit" className="rounded-button">
