f4d701816ab161b54434bac2dc71fcec687ccfbf
Eric599 <ericcuenat@Florences-Air.wifi.unb.ca>
2024-10-04 16:31:21 -0300

    feat(SCRUM-28): Add front end auth routing

---
+++ client/src/state/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from 'react'
+
+const AuthContext = createContext()
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null)
+  const login = userToken => {
+    setToken(userToken)
+  }
+  const logout = () => {
+    setToken(null)
+  }
+  const isAuthenticated = !!token
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
