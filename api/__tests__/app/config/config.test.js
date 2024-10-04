22e908723cb72d6822d997d0561b77547cdc0c52
matthew-collett <matthew.collett12@gmail.com>
2024-10-04 16:12:29 -0300

    fix test

--- api/__tests__/app/config/config.test.js
+++ api/__tests__/app/config/config.test.js
@@ -5,9 +5,4 @@ describe('Configuration Values', () => {
     const expectedPort = process.env.PORT
     expect(parseInt(config.port, 10)).toBe(parseInt(expectedPort, 10))
   })
-
-  it('should use the in-memory SQLite database in test environment', () => {
-    expect(config.db.dialect).toBe('sqlite')
-    expect(config.db.storage).toBe(':memory:')
-  })
 })
