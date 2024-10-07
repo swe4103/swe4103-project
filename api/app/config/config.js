dcd87f7c62ecc90f7288d5d74915945ec0c0625a
matthew-collett <matthew.collett12@gmail.com>
2024-10-07 17:54:28 -0300

    Fix tests

--- api/app/config/config.js
+++ api/app/config/config.js
@@ -2,11 +2,29 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  jwtLoginSecret: process.env.JWT_LOGIN_SECRET,
-  jwtInviteSecret: process.env.JWT_INVITE_SECRET,
-  // add more things over time
+const env = process.env.NODE_ENV || 'development'
+
+const configs = {
+  development: {
+    port: process.env.PORT || 3000,
+    jwtLoginSecret: process.env.JWT_LOGIN_SECRET || 'dev_login_secret',
+    jwtInviteSecret: process.env.JWT_INVITE_SECRET || 'dev_invite_secret',
+  },
+  test: {
+    port: process.env.PORT || 3000,
+    jwtLoginSecret: process.env.JWT_LOGIN_SECRET || 'test_login_secret',
+    jwtInviteSecret: process.env.JWT_INVITE_SECRET || 'test_invite_secret',
+  },
+  production: {
+    port: process.env.PORT || 3000,
+    jwtLoginSecret: process.env.JWT_LOGIN_SECRET,
+    jwtInviteSecret: process.env.JWT_INVITE_SECRET,
+  },
 }
+
+const config = {
+  env,
+  ...configs[env],
+}
+
 export default config
