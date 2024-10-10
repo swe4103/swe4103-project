e4120a67b3392c7cedcccbf046677d106ec992d2
Aidan Foster <132408783+aidanfoster1@users.noreply.github.com>
2024-10-10 10:10:08 -0300

    feat(SCRUM-33): Add auth context and front end routing. (#15)
    Co-authored-by: Eric599 <ericcuenat@Florences-Air.wifi.unb.ca>
    Co-authored-by: Aidan <aidan@Aidans-MacBook-Air.local>
    Co-authored-by: Matthew J. Collett <matthew.collett12@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import './App.css'
+import Home from './components/Home/Home.jsx'
+import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes'
+import AuthProvider from './state/AuthProvider/AuthProvider'
 import LoginForm from './views/LoginForm/LoginForm'
 import RegisterForm from './views/RegistrationForm/RegisterForm'
+import './App.css'
 
 const App = () => {
-  const [isRegister, setIsRegister] = useState(false) // Toggle between login and signup
-
   return (
-    <div className="app-container">
-      {isRegister ? (
-        <RegisterForm setIsRegister={setIsRegister} /> // Show SignUpForm
-      ) : (
-        <LoginForm setIsRegister={setIsRegister} /> // Show LoginForm
-      )}
-    </div>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoutes />}>
+            {/* Dummy component for testing purposes */}
+            <Route path="/home" element={<Home />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </Router>
   )
 }
 
--- client/src/components/HelloWorld/HelloWorld.jsx
+++
@@ -1,5 +0,0 @@
-const HelloWorld = () => {
-  return <h1>Hello World!</h1>
-}
-
-export default HelloWorld
