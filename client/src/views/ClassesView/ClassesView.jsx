ada653e03d12a67af405f7a4c1ff8f284f1ea3e7
LucasSavoie <h4etj@unb.ca>
2024-10-22 13:05:46 -0300

    Added Pie Chart for total team joy rating

--- client/src/views/ClassesView/ClassesView.jsx
+++ client/src/views/ClassesView/ClassesView.jsx
@@ -1,12 +1,22 @@
 import AggregateJoyRatingsChart from '../../components/StudentJoyChart/AggregateStudentJoyChart'
 import IndividualStudentJoyChart from '../../components/StudentJoyChart/IndividualStudentJoyChart'
+import TotalTeamJoyChart from '../../components/StudentJoyChart/TotalTeamJoyChart'
 import StudentJoyForm from '../../components/TeamJoyRating/StudentJoyForm'
 const ClassesView = () => {
   return (
     <>
-      <StudentJoyForm />
-      <AggregateJoyRatingsChart />
-      <IndividualStudentJoyChart />
+      <div>
+        <StudentJoyForm />
+      </div>
+      <div>
+        <AggregateJoyRatingsChart />
+      </div>
+      <div>
+        <IndividualStudentJoyChart />
+      </div>
+      <div>
+        <TotalTeamJoyChart />
+      </div>
     </>
   )
 }
