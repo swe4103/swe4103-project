534d2f3e85ba8b2714545579c5ac3203311856f4
alexgroomUNB <r5fsz@unb.ca>
2024-10-23 09:52:47 -0300

    added confirmation email function in the email service

--- api/app/services/emailService.js
+++ api/app/services/emailService.js
@@ -4,7 +4,7 @@ const { EmailClient } = require('@azure/communication-email')
 
 const emailClient = new EmailClient(config.communicationService.connectionString)
 
-async function sendRegistrationEmail(userEmail, registrationLink, className) {
+export const sendRegistrationEmail = async (userEmail, registrationLink, className) => {
   try {
     const emailMessage = {
       senderAddress: config.communicationService.emailDomainName, // approved domain
@@ -30,6 +30,28 @@ async function sendRegistrationEmail(userEmail, registrationLink, className) {
   }
 }
 
-module.exports = {
-  sendRegistrationEmail,
+export const sendConfirmationEmail = async (userEmail, className) => {
+  try {
+    const emailMessage = {
+      senderAddress: config.communicationService.emailDomainName, // approved domain
+      content: {
+        subject: 'Registration Success',
+        plainText: `You have successfully registered for ${className} on Flow Board!`,
+        html: `<p>You have successfully registered for ${className} on Flow Board!</p>`,
+      },
+      recipients: {
+        to: [
+          {
+            address: userEmail,
+            displayName: 'New User',
+          },
+        ],
+      },
+    }
+
+    const response = await emailClient.send(emailMessage)
+    console.log(`Email sent! Operation ID: ${response.messageId}`)
+  } catch (error) {
+    console.error('Error sending email:', error)
+  }
 }
