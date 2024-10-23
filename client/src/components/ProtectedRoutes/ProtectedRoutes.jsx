b3acb3f50aa783b7d5e319ae7c6d0284464e47d5
Aidan <aidan@raphael.wifi.unb.ca>
2024-10-23 18:20:53 -0300

    Adding admin view.

--- client/src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,15 +1,27 @@
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet, Navigate, useLocation } from 'react-router-dom'
 
-import { useAuth } from '../../state/AuthProvider/AuthProvider.jsx'
+import { useAuth } from '../../state/AuthProvider/AuthProvider'
 
 const ProtectedRoutes = () => {
   const { user, isLoading } = useAuth()
+  const location = useLocation()
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div> // Handle loading state
   }
 
-  return user ? <Outlet /> : <Navigate to="/login" />
+  // If the user is not logged in, redirect to login
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} />
+  }
+
+  // If the user is an admin, restrict access to only the admin section
+  if (user.user.role === 'ADMIN') {
+    return <Navigate to="/admin" />
+  }
+
+  // Allow regular users to access other protected routes
+  return <Outlet />
 }
 
 export default ProtectedRoutes
